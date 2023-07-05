dcb2761d4bc035ad8eef411f5d9f4a1a053201ab
Diego Gava <diego.gava15@gmail.com>
2023-07-05 18:44:55 -0300

    excluído componente SearchBar, implementada função de busca no Home- requisito 5

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,8 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 function Layout() {
   return (
     <div>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-      </nav>
       <main>
         <Outlet />
       </main>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,80 @@
-import { useState } from 'react';
-import SearchBar from '../components/SearchBar';
-import ProductList from '../components/ProductList';
+import { useState, useEffect, ChangeEvent, FormEvent } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+
+interface Product {
+  id: string;
+  title: string;
+  price: number;
+  thumbnail: string;
+}
 
 function Home() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      setLoading(true);
+      const response = await getProductsFromCategoryAndQuery('', searchTerm);
+      const fetchedProducts = response.results;
+      setProducts(fetchedProducts);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      setLoading(false);
+    }
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    if (!searchTerm) {
+      setProducts([]);
+    }
+  }, [searchTerm]);
 
   return (
     <div>
-      <SearchBar setSearchTerm={ setSearchTerm } />
-      {searchTerm ? (
-        <ProductList
-          searchTerm={ searchTerm }
-          data-testid="product"
+      <form onSubmit={ handleSearch }>
+        <input
+          data-testid="query-input"
+          type="text"
+          placeholder="Digite o termo de pesquisa"
+          value={ searchTerm }
+          onChange={ handleInputChange }
         />
+        <button type="submit" data-testid="query-button">
+          Buscar
+        </button>
+      </form>
+
+      {loading ? (
+        <p>Carregando...</p>
       ) : (
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <>
+          {products.length === 0 && (
+            <p>Nenhum produto foi encontrado</p>
+          )}
+          {products.length > 0 && (
+            <div>
+              {products.map((product) => (
+                <div
+                  key={ product.id }
+                  data-testid="product"
+                >
+                  <h3>{product.title}</h3>
+                  <img src={ product.thumbnail } alt={ product.title } />
+                  <p>{`R$ ${product.price}`}</p>
+                </div>
+              ))}
+            </div>
+          )}
+        </>
       )}
     </div>
   );
