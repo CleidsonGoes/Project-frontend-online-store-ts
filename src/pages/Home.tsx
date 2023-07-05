39801e5cfd2d45bd4201a1bb1e1c3f63f01f59d1
Hiago <hiagoisoppotrajano@gmail.com>
2023-07-05 18:14:26 -0300

    Implementando a lista de categorias para navegação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import SearchBar from '../components/SearchBar';
+import CategoryList from '../components/CategoryList';
 
 function Home() {
   const [searchTerm, setSearchTerm] = useState('');
 
   return (
     <div>
+      <CategoryList />
       <SearchBar setSearchTerm={ setSearchTerm } />
       {searchTerm ? (
-        <p>Lista de produtos</p>
+        <p>Você ainda não realizou uma busca</p>
       ) : (
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
