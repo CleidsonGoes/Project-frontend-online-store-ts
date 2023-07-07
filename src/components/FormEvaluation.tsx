b557b136a21b86f4b463d7166740481e86a3cd44
Diego Gava <diego.gava15@gmail.com>
2023-07-07 18:14:58 -0300

    Implementando requisito 12

--- src/components/FormEvaluation.tsx
+++ src/components/FormEvaluation.tsx
@@ -1,11 +1,11 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
 
 type InputRadio = React.ChangeEvent<HTMLInputElement>;
 type InputButton = React.MouseEvent<HTMLButtonElement>;
 type ProductReview = {
-  email: string,
-  text: string,
-  rating: string,
+  email: string;
+  text: string;
+  rating: string;
 };
 
 function FormEvaluation() {
@@ -14,19 +14,39 @@ function FormEvaluation() {
   const [inputTextArea, setInputTextArea] = useState('');
   const [reviews, setReviews] = useState<ProductReview[]>([]);
   const [emailInvalido, setEmailInvalido] = useState(false);
+  const [formInvalido, setFormInvalido] = useState(false);
 
   const validationEmail = () => {
     const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
     return regexEmail.test(emailValue);
   };
 
-  if (validationEmail()) setEmailInvalido(true);
+  const saveReviewsToLocalStorage = (updatedReviews: ProductReview[]) => {
+    localStorage.setItem('product-reviews', JSON.stringify(updatedReviews));
+  };
 
-  const handleSubmit = (event:InputButton) => {
+  useEffect(() => {
+    const savedReviews = localStorage.getItem('product-reviews');
+    if (savedReviews) {
+      setReviews(JSON.parse(savedReviews));
+    }
+  }, [setReviews]);
+
+  useEffect(() => {
+    saveReviewsToLocalStorage(reviews);
+  }, [reviews]);
+
+  const handleSubmit = (event: InputButton) => {
     event.preventDefault();
 
-    const newReview:ProductReview = {
+    if (emailValue === '' || check === '' || inputTextArea === '') {
+      setFormInvalido(true);
+      return;
+    }
+
+    if (validationEmail()) setEmailInvalido(true);
+
+    const newReview: ProductReview = {
       email: emailValue,
       text: inputTextArea,
       rating: check,
@@ -36,13 +56,35 @@ function FormEvaluation() {
 
     setEmailValue('');
     setInputTextArea('');
+    setFormInvalido(false);
   };
 
-  const handleRadioChange = (event:InputRadio) => {
+  const handleRadioChange = (event: InputRadio) => {
     setCheck(event.target.value);
   };
+
   const handleTextAreaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setInputTextArea(event.target.value);
+  };
+
+  const renderRadioButtons = () => {
+    const ratings = ['1', '2', '3', '4', '5'];
+
+    return ratings.map((rating, index) => (
+      <div key={ rating }>
+        <input
+          required
+          type="radio"
+          id={ `star${rating}` }
+          name="rating"
+          value={ rating }
+          data-testid={ `${index + 1}-rating` }
+          checked={ check === rating }
+          onChange={ handleRadioChange }
+        />
+        <label htmlFor={ `star${rating}` }>&#9733;</label>
+      </div>
+    ));
   };
 
   return (
@@ -58,69 +100,14 @@ function FormEvaluation() {
             onChange={ (event) => setEmailValue(event.target.value) }
           />
         </label>
-        <div className="rating">
-          <input
-            required
-            type="radio"
-            id="star5"
-            name="rating"
-            value="5"
-            data-testid="5-rating"
-            checked={ check === '5' }
-            onChange={ handleRadioChange }
-          />
-          <label htmlFor="star5">&#9733;</label>
-          <input
-            required
-            type="radio"
-            id="star4"
-            name="rating"
-            value="4"
-            data-testid="4-rating"
-            checked={ check === '4' }
-            onChange={ handleRadioChange }
-          />
-          <label htmlFor="star4">&#9733;</label>
-          <input
-            required
-            type="radio"
-            id="star3"
-            name="rating"
-            value="3"
-            data-testid="3-rating"
-            checked={ check === '3' }
-            onChange={ handleRadioChange }
-          />
-          <label htmlFor="star3">&#9733;</label>
-          <input
-            required
-            type="radio"
-            id="star2"
-            name="rating"
-            value="2"
-            data-testid="2-rating"
-            checked={ check === '2' }
-            onChange={ handleRadioChange }
-          />
-          <label htmlFor="star2">&#9733;</label>
-          <input
-            required
-            type="radio"
-            id="star1"
-            name="rating"
-            value="1"
-            data-testid="1-rating"
-            checked={ check === '1' }
-            onChange={ handleRadioChange }
-          />
-          <label htmlFor="star1">&#9733;</label>
-        </div>
+        <div className="rating">{renderRadioButtons()}</div>
         <br />
         <label htmlFor="">
           <textarea
-            name=""
-            id=""
+            name="product-detail-evaluation"
+            id="product-detail-evaluation"
             data-testid="product-detail-evaluation"
+            value={ inputTextArea }
             onChange={ handleTextAreaChange }
           />
         </label>
@@ -135,28 +122,20 @@ function FormEvaluation() {
           </button>
         </label>
       </form>
-      {
-        !emailInvalido && !check ? (<h3 data-testid="error-msg">Campos inválidos</h3>)
-          : reviews.map((review, index) => (
-            <div key={ index }>
-              <p data-testid="review-card-email">
-                Email:
-                {' '}
-                {review.email}
-              </p>
-              <p data-testid="review-card-rating">
-                Rating:
-                {' '}
-                {review.rating}
-              </p>
-              <p data-testid="review-card-evaluation">
-                Comment:
-                {' '}
-                {review.text}
-              </p>
-            </div>
-          ))
-        }
+      {formInvalido && (
+        <h3 data-testid="error-msg">Campos obrigatórios não preenchidos</h3>
+      )}
+      {!emailInvalido && !check ? (
+        <h3 data-testid="error-msg">Campos inválidos</h3>
+      ) : (
+        reviews.map((review, index) => (
+          <div key={ index }>
+            <p data-testid="review-card-email">{`Email: ${review.email}`}</p>
+            <p data-testid="review-card-rating">{`Rating: ${review.rating}`}</p>
+            <p data-testid="review-card-evaluation">{`Comment: ${review.text}`}</p>
+          </div>
+        ))
+      )}
     </>
   );
 }
